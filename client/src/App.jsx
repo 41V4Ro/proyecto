cf5f2fd2c8a054dd677286d6df7b6e3c88ff231e
alvaro <asdf>
2023-02-07 11:54:38 +0100

    timer hecho

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,24 +18,24 @@ function Home(){
   
 
   const words = [
-    "árbol", "animal", "avisan","amarillo","amistad","antena",
-    "brazo", "bicicleta", "burro","birra","brutal","bestias",
-    "cazo", "creían", "chorizo", "crecer","cavernas","campo",
-    "días", "duende","dinámico","dócil","dios","dudas",
-    "enredar","empatía","enfrentó","entretener","ética",
-    "fallecer", "familiares","favorito", "fuente","fantástico",
-    "grande","gritos","grulla", "gazpacho","gamba",
-    "habían","hombro", "hebras", "hambre","híbrido",
-    "ignorante","ímpetu", "independencia", "idea", "inferior",
-    "jabalí","jalapeño","jugadores", "jamón","justa", "jugado", "japón",
+    "árbol", "animal", "avisan","amarillo","amistad","antena", "asno", "altura", "arte","arrojar","asistir","archivo","atacar",
+    "brazo", "bicicleta", "burro","birra","brutal","bestias","bocina","borde","belleza","blando","binario","beber","bebé",
+    "cazo", "creían", "chorizo", "crecer","cavernas","campo","crema","cuerpo","clase","cima","cortar", "cristiano",
+    "días", "duende","dinámico","dócil","dios","dudas", "diario","débil","diablo","dinamita","dedo","dentadura","Dinamarca",
+    "enredar","empatía","enfrentó","entretener","ética","elefante", "Etiopía","ese","ellas","emprender",
+    "fallecer", "familiares","favorito", "fuente","fantástico", "freno","fábula","Finlandia","feo",
+    "grande","gritos","grulla", "gazpacho","gamba", "ganso","girar","gato","guía","gracias",
+    "habían","hombro", "hebras", "hambre","híbrido", "hace","hábitos","Honduras","hospital","haber",
+    "ignorante","ímpetu", "independencia", "idea", "inferior","intento","interno","iterar","insignia",
+    "jabalí","jalapeño","jugadores", "jamón","justa", "jugado", "juez","Japón",
     "kilómetro","karaoke", "kétchup", 
-    "limonada","linterna","luciérnaga", "láser","lista",
-    "madre","muertos","mirar", "música","mentira",
-    "navidad","nogal","noche", "noche", "nietos",
+    "limonada","linterna","luciérnaga", "láser","lista","lluvia","lápiz","libro","luna","leer","lagarto","lámpara","lujoso",
+    "madre","muertos","mirar", "música","mentira","marisco","mueble","manzana","misterio","magia",
+    "navidad","nogal","noche", "noche", "nietos","naranja","nación","nido","nuevo","nacimiento",
     "ñu","ñora",
-    "ópera","ocioso","ombligo",
-    "pirámide","palabras","puerro",
-    "queso","quieres","quebrar",
+    "ópera","ocioso","ombligo","ojo","otoño","oeste","oportunidad","orden",
+    "pirámide","palabras","puerro", "pájaro","papel","paz","página","planeta","pulgar","playa","pelea",
+    "queso","quieres","quebrar", "quinto","qué",
     "rata","rompieron","retrasarse",
     "sátira","sobrio","separar",
     "trueque","trampas","tirar",
@@ -46,6 +46,7 @@ function Home(){
     "yema","yo","yendo",
     "zapato","zumo","zote"
   ]
+  
   /* let [previousWord, setPreviousWord] = useState(""); */
   let [word, setWord] = useState(words[Math.floor(Math.random() * words.length)]);
   let [nextWord, setNextWord] = useState(words[Math.floor(Math.random() * words.length)])
@@ -55,9 +56,45 @@ function Home(){
   let [score, setScore] = useState(0);
   let [key, setKey] = useState("");
 
-  const keyChange = event =>{
+
+  const keyChange = event =>{ // detectar las teclas pulsadas
     setKey(event.key);
   }
+
+  //contador
+  let [time, setTime] = useState(60);
+ 
+  let [timer, setTimer] = useState(undefined);
+  const startGame = () =>{
+    clearInterval(timer);
+    const input = document.querySelector("input");
+    console.log(timer)
+    setTimer(setInterval(()=>{
+      input.disabled = false;
+      input.focus();
+      
+      setTime(time=>{
+        if(time <1){
+          input.disabled = true;
+          clearInterval(timer);
+          return time;
+        }else{
+          return time -1;
+        }
+      });
+      
+    },1000)) 
+  }
+  const restartGame = ()=>{
+    setUserInput("");
+    setTime(60);
+    setCorrect(0);
+    setIncorrect(0);
+    setWord(words[Math.floor(Math.random() * words.length)])
+    setNextWord(words[Math.floor(Math.random() * words.length)])
+    startGame();
+  }
+  
   const compare = event => {
     setUserInput(event.target.value);
     if(key === " "){
@@ -80,7 +117,7 @@ function Home(){
     localStorage.removeItem("nombre");
     navigate("/login");
   }
-  let user = localStorage.getItem("nombre") ?<><button>{localStorage.getItem("nombre")} <BsFillPersonFill/> </button><a href='/login'>Log out <BsBoxArrowLeft/></a></>: <button onClick={logOut}>Login</button>;
+  let user = localStorage.getItem("nombre") ?<><button>{localStorage.getItem("nombre")} <BsFillPersonFill/> </button><a onClick={logOut} href='/login'>Cerrar sesión <BsBoxArrowLeft/></a></>: <button >Login</button>;
   
   return (
     <div className="App">
@@ -97,8 +134,10 @@ function Home(){
             <h1>{word}</h1>
             <h3>{nextWord}</h3>
           </div>
-          <input type="text" value={userInput} onKeyDown={keyChange} onChange={compare}/>
-          <p>Bien: {correct} Mal: {incorrect}</p>
+          <input disabled type="text" value={userInput} onKeyDown={keyChange} onChange={compare} />
+          <button onClick={startGame}>Iniciar</button>
+          <button onClick={restartGame}>Reiniciar</button>
+          <p>Bien: {correct} Mal: {incorrect} Tiempo: {time}</p>
         </div>
       </main>
     </div>
